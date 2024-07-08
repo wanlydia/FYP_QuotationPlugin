b98074cd9c6abf06011df1d98c226fc29aa3289e
CrossoverRed <gravityfalls9681@gmail.com>
2024-07-08 17:03:55 +0800

    Test 2

--- renoku-calculator.php
+++ renoku-calculator.php
@@ -1,5 +1,11 @@
 <?php
 /*
-
+Plugin Name: Calculator Plugin (RP)
+Plugin URI: https://renoku2.azharapp.com/reviews/
+Description: Calculator for Renoku's Website 
+Version: 1.0
+Author:Lydia, Chloe
+Author URI: https://github.com/wanlydia, https://github.com/CrossoverRed 
+License: GPL2
 */
 ?>
