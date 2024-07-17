3cfaf7bc2c5d4b4e9751db40afb4e7e1b5b577a8
22026074 <22026074@myrp.edu.sg>
2024-07-17 22:59:09 +0800

    Removed js from calc-kitchen.php

--- calc-kitchen.php
+++ calc-kitchen.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: Calculator Kitchen Plugin (RP)
 Plugin URI: https://renoku2.azharapp.com/reviews/
-Description: Calculator for Renoku's Website 
+Description: Calculator for Renoku's Website
 Version: 1.0
 Author: Lydia, Chloe
-Author URI: https://github.com/wanlydia, https://github.com/CrossoverRed 
+Author URI: https://github.com/wanlydia, https://github.com/CrossoverRed
 License: GPL2
 */
 
@@ -33,31 +33,12 @@ function bvd_display_buttons() {
             <button class="bvd-button kitchen-set2" data-value="30">Extensive</button>
         </div>
         <div id="kitchen-set2-text" class="button-text"></div>
-        <p>Total Size of Renovated Area</p> 
+        <p>Total Size of Renovated Area</p>
         <input type="number" id="bvd-input" placeholder="Enter...">
         <button id="bvd-submit">Submit</button>
         <div id="bvd-result"></div>
     </div>
-    <script type="text/javascript">
-        jQuery(document).ready(function($) {
-            $('#bvd-submit').click(function() {
-                var inputValue = parseFloat($('#bvd-input').val());
-                if (isNaN(inputValue)) {
-                    $('#bvd-result').text('Please enter a valid number');
-                    return;
-                }
-
-                if (selectedValueSet1 !== null || selectedValueSet2 !== null) {
-                    var sum = (selectedValueSet1 || 0) + (selectedValueSet2 || 0);
-                    var result = sum * inputValue;
-                    $('#bvd-result').text('Result: $' + result);
-                    window.location.href = 'URL_TO_CALC_LIVING_PAGE'; // Replace with actual URL
-                } else {
-                    $('#bvd-result').text('Please select at least one button');
-                }
-            });
-        });
-    </script>
     <?php
 }
 add_shortcode('bvd_buttons', 'bvd_display_buttons');
+?>
