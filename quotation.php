035c32f72325da48ea1c88834c73d0a789f90f61
22026074 <22026074@myrp.edu.sg>
2024-07-21 20:28:57 +0800

    renamed files

---
+++ quotation.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Quotation Plugin (RP)
+Plugin URI: https://renoku2.azharapp.com/reviews/
+Description: Calculator for Renoku's Website
+Version: 1.0
+Author: Lydia, Chloe
+Author URI: https://github.com/wanlydia, https://github.com/CrossoverRed
+License: GPL2
+*/
